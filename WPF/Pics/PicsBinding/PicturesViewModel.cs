787a5b313dccfbaa8ad79c4bc783d4d6d77ffbc9
Kyrylo Bezpalyi <compilyator@hotmail.com>
2016-09-17 22:55:56 +0300

    WPF MVVM example

---
+++ WPF/Pics/PicsBinding/PicturesViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+
+namespace PicsBinding
+{
+    public class PicturesViewModel
+    {
+        private string folderName = "Pictures";
+
+        public ObservableCollection<Uri> FileNames { get; set; }
+
+        public ICommand GetFileNames { get; set; }
+
+        public PicturesViewModel()
+        {
+            GetFileNames = new RelayCommand(o => LoadPictures());
+            FileNames = new ObservableCollection<Uri>();
+        }
+
+        private void LoadPictures()
+        {
+            var fileNames = Directory.GetFiles(folderName).Select(fullName => Path.GetFileName(fullName));
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            foreach (var fileName in fileNames)
+            {
+                var path = Path.Combine(baseDir, folderName, fileName);
+                FileNames.Add(new Uri(path));
+            }
+        }
+    }
+}
---
+++ WPF/Pics/PicsManual/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace PicsManual
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string folderName = "Pictures";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            var fileNames = Directory.GetFiles(folderName).Select(fullName => Path.GetFileName(fullName));
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            foreach (var fileName in fileNames)
+            {
+                var path = Path.Combine(baseDir, folderName, fileName);
+                var uri = new Uri(path);
+                var image = new Image {Source = new BitmapImage(uri)};
+                PicsListView.Items.Add(image);
+            }
+        }
+    }
+}
