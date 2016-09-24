c422f25ef8b749b28258533764e50d281168c7dd
Kyrylo Bezpalyi <compilyator@hotmail.com>
2016-09-24 15:25:01 +0300

    Add wpf animation example

---
+++ WPF/TriggersAndAnimations/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace TriggersAndAnimations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ViewModel
+    {
+        public bool Checked { get; set; }
+        
+    }
+}
