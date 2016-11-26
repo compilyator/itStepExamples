032343a89f3dc20353b7afa906816bc0ab7c6a43
Kyrylo Bezpalyi <compilyator@hotmail.com>
2016-11-26 14:10:32 +0200

    Smtp example

---
+++ NetworkProgramming/SmtpExample/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SmtpExample
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Mail;
+    using System.Text;
+
+    class Program
+    {
+        private static SmtpClient GetClient()
+        {
+            var smtpClient = new SmtpClient();
+            return smtpClient;
+        }
+
+        private static MailMessage GetMessage()
+        {
+            var message = new MailMessage
+                              {
+                                  From = new MailAddress("bigBill@microsoft.com", "Bill Gates"),
+                                  Subject = "Smtp test",
+                                  SubjectEncoding = Encoding.UTF8,
+                                  Body = "<h3>Привет!</h3><br>Вот девахи которых ты просил",
+                                  IsBodyHtml = true
+                              };
+            var email = ConfigurationManager.AppSettings["ToEmail"];
+            var userName = ConfigurationManager.AppSettings["ToUserName"];
+            message.To.Add(new MailAddress(email, userName));
+            Directory.GetFiles($@"{Directory.GetCurrentDirectory()}\Images")
+                .ToList()
+                .ForEach(file => message.Attachments.Add(new Attachment(file)));
+
+            return message;
+        }
+
+        static void Main()
+        {
+            var smtpClient = GetClient();
+            var message = GetMessage();
+            smtpClient.Send(message);
+            Console.WriteLine("Success!");
+        }
+    }
+}
